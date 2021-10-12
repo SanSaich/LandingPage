5896cbd7c684b881b355d6e242a41d3cc4fb6b95
SanSaich <kurdenkov93@gmail.com>
2021-10-12 22:06:01 +0700

    скрыл скроллап на экранах < 767

--- scrollup.js
+++ scrollup.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 100 && $(window).width() > 767) {
       $(".scrollup").fadeIn();
     } else {
       $(".scrollup").fadeOut();
